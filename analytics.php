c0350c3bf1d16b30e5fbd76c29a04c5723c999b5
xmeunier <xmeunier@gmail.com>
2025-11-22 10:08:08 +0100

    Add files via upload

---
+++ analytics.php
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F6Z5X90D7K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F6Z5X90D7K');
+</script>
