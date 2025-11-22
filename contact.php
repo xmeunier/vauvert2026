7e3b49e6412dcc5a2da859fc87761181f445d4d9
xmeunier <xmeunier@gmail.com>
2025-11-22 09:52:17 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -39,6 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <?php include 'meta_tags.php'; ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
